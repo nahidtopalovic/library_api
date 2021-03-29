4e10c02e042fbed6bbf378e54274fbbb7a2dacd1
angrypotato10 <nahidtop@gmail.com>
2021-03-29 20:07:34 +0100

    reverted to http

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 //This enables https
 
-const https = require('https')
-const fs = require('fs')
+// const https = require('https')
+// const fs = require('fs')
 
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
-}
+// const options = {
+//   key: fs.readFileSync('key.pem'),
+//   cert: fs.readFileSync('cert.pem'),
+// }
 
 //normal stuff and ports
 // removed options parameter
-const server = https.createServer(app)
+const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
