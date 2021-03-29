cfb1ec547f17fecfb192003f1dc2955fa9953cb8
angrypotato10 <nahidtop@gmail.com>
2021-03-29 19:35:45 +0100

    changed server config

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const app = require("./app");
-const http = require("http");
-const config = require("./utils/config");
-const logger = require("./utils/logger");
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 //This enables https
 
-const https = require("https");
-const fs = require("fs");
+const https = require('https')
+const fs = require('fs')
 
 const options = {
-    key: fs.readFileSync("key.pem"),
-    cert: fs.readFileSync("cert.pem")
-  };
-
-
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+}
 
 //normal stuff and ports
-const server = https.createServer(options, app);
+// removed options parameter
+const server = https.createServer(app)
 
 server.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`);
-});
+  logger.info(`Server running on port ${config.PORT}`)
+})
