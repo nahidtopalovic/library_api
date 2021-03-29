4ebb775e748ca9d56d9252d3064693af9269782f
angrypotato10 <nahidtop@gmail.com>
2021-03-29 20:37:53 +0100

    removed https

--- index.js
+++ index.js
@@ -3,18 +3,6 @@ const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-//This enables https
-
-// const https = require('https')
-// const fs = require('fs')
-
-// const options = {
-//   key: fs.readFileSync('key.pem'),
-//   cert: fs.readFileSync('cert.pem'),
-// }
-
-//normal stuff and ports
-// removed options parameter
 const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
