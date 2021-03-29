33e50154ee9070abe51466f9ea465528a37272c6
David Haunschild <davidh99@hotmail.de>
2021-03-29 17:43:03 +0200

    htts upgrade & .md

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const http = require("http");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 
-const server = http.createServer(app);
+//This enables https
+
+const https = require("https");
+const fs = require("fs");
+
+const options = {
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem")
+  };
+
+//normal stuff
+const server = https.createServer(options, app);
 
 server.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`);
